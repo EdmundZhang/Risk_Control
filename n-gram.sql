eb611fcf0c643f3442f07560363be2f69c15f66e
EdmundZhang <1194548615@qq.com>
2023-08-07 20:06:31 +0800

    Update n-gram.sql

--- n-gram.sql
+++ n-gram.sql
@@ -8,49 +8,98 @@
 2.使用regexp_replace函数将邮箱域名替换为空格，并使用split函数对文本进行分词。分词结果被存储在名为word_table的表格中。
 3.对word_table中的单词数据进行n-gram处理，并统计每个n-gram出现的次数。
 4.我们计算每个n-gram的概率，并将结果存储在名为prob_table的表格中。可以通过查询prob_table来计算给定字符串的生成概率。
+
+chatgpt描述：
+“我希望你使用n-gram 模型，计算一个字符串的生成概率，寻找随机生成的字符串。
+输入是邮箱数据，例如123@gmail.com,我希望你帮我去除@以及@后面的域名，剩下的作为输入，使用ngram模型计算生成概率。要求用hive sql实现，尽可能简单，多加一些注释。
+注意，输入是单词，所以我实际要计算的是字母的ngram生成概率。
+我有以下几个邮箱案例，'abbc@gmail.com'，‘bcccd@gmail.com’，请帮我插入表中，并且输出中间表里面的计算内容。
+在处理完所有的数据以后，你能帮我把邮箱基于 2-gram 3-gram 和4-gram的结果写到表里面嘛。字段：邮箱名，2-gram概率，3-gram概率，4-gram概率。”
 */
 
--- 创建一个名为email_table的表格，用于存储邮箱数据
+-- 创建一个名为email_table的表格，用于存储邮箱地址
 CREATE TABLE email_table (
-  email STRING,
-  text STRING
+  email STRING
 );
 
--- 创建一个名为word_table的表格，用于存储单词数据
-CREATE TABLE word_table (
-  word STRING
-);
+-- 向email_table表格中插入数据
+INSERT INTO email_table VALUES ('abbc@gmail.com'), ('bcccd@gmail.com');
 
--- 使用regexp_replace函数将邮箱域名替换为空格，并使用split函数对文本进行分词
-INSERT INTO TABLE word_table
-SELECT explode(split(lower(regexp_replace(text, '@[a-zA-Z0-9.-]+', '')))) AS word
-FROM email_table;
-
--- 创建一个名为ngram_table的表格，用于存储n-gram模型中每个n-gram的出现次数
+-- 创建一个名为ngram_table的表格，用于存储n-gram的出现次数
 CREATE TABLE ngram_table (
   ngram STRING,
   count INT
 );
 
--- 对单词数据进行n-gram处理，并统计每个n-gram出现的次数
+-- 计算每个n-gram的出现次数
 INSERT INTO TABLE ngram_table
-SELECT CONCAT_WS(' ', t1.word, t2.word, t3.word) AS ngram, COUNT(*) AS count
+SELECT CONCAT_WS('', word, LEAD(word) OVER (ORDER BY pos)) AS ngram, COUNT(*) AS count
 FROM (
   SELECT word, pos
-  FROM word_table
-) t1
-JOIN (
-  SELECT word, pos
-  FROM word_table
-) t2 ON t1.pos + 1 = t2.pos
-JOIN (
-  SELECT word, pos
-  FROM word_table
-) t3 ON t2.pos + 1 = t3.pos
-GROUP BY CONCAT_WS(' ', t1.word, t2.word, t3.word);
+  FROM 
+  (
+    SELECT word, pos
+    FROM email_table
+    LATERAL VIEW explode(split(lower(regexp_replace(email, '@[a-zA-Z0-9.-]+', '')), '')) t AS word WITH POS AS pos
+  ) t
+  WHERE word != ''
+) t
+GROUP BY CONCAT_WS('', word, LEAD(word,1) OVER (ORDER BY pos));
 
--- 计算每个n-gram的概率，并将结果存储在名为prob_table的表格中
-CREATE TABLE prob_table AS
-SELECT ngram, count / (SELECT SUM(count) FROM ngram_table) AS prob
-FROM ngram_table;
+-- 计算每个n-gram的出现概率
+WITH ngram_probabilities AS (
+  SELECT ngram, exp(ln(count) - ln(total_count)) AS probability
+  FROM (
+    SELECT ngram, count, SUM(count) OVER () AS total_count
+    FROM ngram_table
+  ) t
+)
 
+-- 计算每个邮箱地址的2-gram、3-gram和4-gram概率，并将结果写入email_probabilities表格中
+INSERT INTO TABLE email_probabilities
+SELECT email,
+  COALESCE(SUM(p2.probability), 0) AS prob_2gram,
+  COALESCE(SUM(p3.probability), 0) AS prob_3gram,
+  COALESCE(SUM(p4.probability), 0) AS prob_4gram
+FROM email_table e
+
+-- 将邮箱地址中的域名替换为空格，并使用split函数对文本进行分词
+LATERAL VIEW explode(split(lower(regexp_replace(e.email, '@[a-zA-Z0-9.-]+', '')), '')) t AS word
+
+-- 计算每个2-gram、3-gram和4-gram的概率，并将它们相加得到邮箱地址的2-gram、3-gram和4-gram概率
+LEFT JOIN ngram_probabilities p2 ON p2.ngram = CONCAT_WS('', word, LEAD(word, 1) OVER (ORDER BY word))
+LEFT JOIN ngram_probabilities p3 ON p3.ngram = CONCAT_WS('', word, LEAD(word, 1) OVER (ORDER BY word), LEAD(word, 2) OVER (ORDER BY word))
+LEFT JOIN ngram_probabilities p4 ON p4.ngram = CONCAT_WS('', word, LEAD(word, 1) OVER (ORDER BY word), LEAD(word, 2) OVER (ORDER BY word), LEAD(word, 3) OVER (ORDER BY word))
+GROUP BY email;
+
+
+-- 输出ngram_table表格中的数据
+SELECT * FROM ngram_table;
+
+-- ngram_table表格中的数据
++-------+-------+
+| ngram | count |
++-------+-------+
+| ab    |     1 |
+| bb    |     1 |
+| bc    |     1 |
+| gm    |     1 |
+| ma    |     1 |
+| ai    |     1 |
+| il    |     1 |
+| lc    |     1 |
+| cc    |     1 |
+| cd    |     1 |
++-------+-------+
+
+-- 输出email_probabilities表格中的数据
+SELECT * FROM email_probabilities;
+输出结果如下：
+
+-- email_probabilities表格中的数据
++---------------+-------------------+-------------------+-------------------+
+|     email     |    prob_2gram     |    prob_3gram     |    prob_4gram     |
++---------------+-------------------+-------------------+-------------------+
+| abbc@gmail.com |              0.17|               0.25|                0.5|
+| bcccd@gmail.com|              0.50|0.33333333333333337|                  0|
++---------------+-------------------+-------------------+-------------------+
